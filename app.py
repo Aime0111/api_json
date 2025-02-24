9c90869235690af8427639691fe02e6013e53a59
Aime <aimeca011.com>
2025-02-24 11:17:36 -0600

    Mostrar todos los alumnos

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return "Hola Mundo"
 
-@app.route("/alumnos")
-def get_alumnos():
-    return "Retornando todos los alumnos"
+# Definición del modelo de la tabla 'estudiantes'
+class Alumno(db.Model):
+    __tablename__ = 'alumnos'
+    no_control = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String)
+    ap_paterno = db.Column(db.String)
+    ap_materno = db.Column(db.String)
+    semestre = db.Column(db.Integer)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    def to_dict(self):
+        return {
+            'no_control': self.no_control,
+            'nombre': self.nombre,
+            'ap_paterno': self.ap_paterno,
+            'ap_materno': self.ap_materno,
+            'semestre': self.semestre,
+        }
+
+# Endpoint para obtener todos los estudiantes
+@app.route('/alumnos', methods=['GET'])
+def listar_alumnos():
+    alumnos = Alumno.query.all()
+    listar_alumnos = []
+    for alumno in alumnos:
+        listar_alumnos.append({
+            'no_control': alumno.no_control,
+            'nombre': alumno.nombre,
+            'ap_paterno': alumno.ap_paterno,
+            'ap_materno': alumno.ap_materno,
+            'semestre': alumno.semestre
+        })
+    return jsonify(listar_alumnos)
